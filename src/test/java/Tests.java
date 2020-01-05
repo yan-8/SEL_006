2ae6d067fb067d2799857bd0af189ec35c851e88
Yan <yan.bondarenko@gmail.com>
2020-01-05 14:45:14 +0200

    Add DataProvider as separate class

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,32 +1,37 @@
-import com.github.javafaker.Faker;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.util.Locale;
-
+@RunWith(DataProviderRunner.class)
 public class Tests extends BaseTest {
-
     @Test
-    public void registrationNewUser () throws Exception {
-        faker = new Faker(Locale.US);
-        String email = faker.name().firstName().toLowerCase() + "_" + System.currentTimeMillis() + "@4qmail.com";
-        String password = faker.lorem().characters(8, 16) + faker.lorem().characters(8, 16);
-
+    @UseDataProvider(value = "validUser", location = DataProviders.class)
+    public void registrationNewUser(User user) throws Exception {
+        // открыть главную страницу
         driver.get("https://www.booking.com");
 
+        // кликаем на кнопку и ждем открытие новое страницы, ждем пока не появится определенный элемент
         driver.findElement(By.xpath(".//li[@id = 'current_account_create']//div[@class = 'sign_in_wrapper']")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//input[@id = 'login_name_register']")));
 
-        driver.findElement(By.xpath(".//input[@id = 'login_name_register']")).sendKeys(email);
+        // вводим логин/логин и жмем на кнопку ОК и ждем пока не появится нужный элемент на другой странице
+        driver.findElement(By.xpath(".//input[@id = 'login_name_register']")).sendKeys(user.getEmail());
         driver.findElement(By.xpath(".//button[@class = 'bui-button bui-button--large bui-button--wide']")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//input[@id = 'password']")));
 
-        driver.findElement(By.xpath(".//input[@id = 'password']")).sendKeys(password);
-        driver.findElement(By.xpath(".//input[@id = 'confirmed_password']")).sendKeys(password);
+        // заполняем пароль и жем пока не появится нужный элемент
+        driver.findElement(By.xpath(".//input[@id = 'password']")).sendKeys(user.getPassword());
+        driver.findElement(By.xpath(".//input[@id = 'confirmed_password']")).sendKeys(user.getPassword());
         driver.findElement(By.xpath(".//button[@class = 'bui-button bui-button--large bui-button--wide']")).click();
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//span[@class = 'user_name_block']")));
 
 //        Thread.sleep(3_000);
+    }
+
+//    @Test
+    public void logInTestUser() {
     }
 }
